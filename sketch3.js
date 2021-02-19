80d4918df2d756ef33743acb409032dcb2482b77
Zhengyan Yu <noel.fish6@gmail.com>
2021-02-19 21:05:44 +0800

    draw falling circles

---
+++ sketch3.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(100);
+}
+
+
+// draw three circles
+// var r = 0
+// function draw() {
+
+//     r = mouseX/2
+//     var brightness = r/2 +40
+//     fill(brightness)
+//     ellipse(width/2, height/3, r, r)
+//     ellipse(width/2, height/2, r, r)
+//     ellipse(width/2, height/6, r, r)  
+// }
+
+// draw dropping circle
+var r = 0
+var x = 50, y = 50
+var vx = 3, vy = 3
+function draw() {
+  // background(0)
+  ellipse(x, y, 50)
+  x = x + vx
+  y = y + vy
+  vy += 0.2
+  if (y > height) {
+    vy = -vy * 0.6
+    y = height
+  }  
+}
