990ebb5ab94d8a3aec66d8a33a8a32379b0a107b
Zhengyan Yu <noel.fish6@gmail.com>
2021-03-07 19:02:38 +0800

    use % to change the color

--- sketch4.js
+++ sketch4.js
@@ -7,6 +7,9 @@ var x = 10, y = 10
 function draw() {
   var w = random(30,80)
   fill(100 + random(155), 59, 59)
+  if (frameCount % 3 == 0){
+    fill('white')
+  }
   rect(x, y, w, 20)
   x += w + 10
   if (x>width){
