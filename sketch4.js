daffd05fe3ebecc563a81c8346cad3f693db8ad8
Zhengyan Yu <noel.fish6@gmail.com>
2021-03-08 21:59:27 +0800

    add three blocks

--- sketch4.js
+++ sketch4.js
@@ -7,8 +7,9 @@ var x = 10, y = 10
 function draw() {
   var w = random(30,80)
   fill(100 + random(155), 59, 59)
-  if (frameCount % 3 == 0){
+  if (frameCount % 7 < 3){
     fill('white')
+    w = 10
   }
   rect(x, y, w, 20)
   x += w + 10
