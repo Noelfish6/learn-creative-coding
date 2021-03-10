c98a562cdba31efed79b5d1673c28e2486d8e983
Zhengyan Yu <noel.fish6@gmail.com>
2021-03-10 22:06:36 +0800

    draw random and continious circles

--- sketch5.js
+++ sketch5.js
@@ -1,11 +1,19 @@
+const { randomBytes } = require("crypto")
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
   background(100)
+  x = width / 2
+  y = height / 2
+  delta = 10
 }
 
-
+var x, y, delta
 function draw() {
-  ellipse(mouseX, mouseY, 20, 20)
-  var colorR = map(mouseX, 0, width, 0, 255, true)
+  delta = mouseX / 50
+  x += random(-delta, delta)
+  y += random(-delta, delta)
+  var colorR = map(mouseX, 0, width-10, 0, 255, true)
   fill(colorR, 0, 0)
+  ellipse(x, y, 100, 100)
 }
