e76876afc1e66b9d3271ed4852b8fdfe3fc2290a
Zhengyan Yu <noel.fish6@gmail.com>
2021-03-14 19:01:59 +0800

    draw repeated rects with random colors

--- sketch7.js
+++ sketch7.js
@@ -1,10 +1,15 @@
 function setup() {
   createCanvas(windowWidth, windowHeight)
-  background(100)
+  background(0)
+  noStroke()
+  for (var x = 0; x < width/2; x+=5){
+    for (var y = 0; y < height/2; y+=5){
+      fill(random(200,255), random(255), random(255))
+      rect(x, y, 40)
+    }
+  }
 }
 
 function draw() {
-  for(var x =0; x<width; x+=50){
-    rect(x, height/2, 50)
-  }
+
 }
