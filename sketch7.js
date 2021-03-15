c679c4933bae6628e61db307ee52198d28fb7a33
Zhengyan Yu <noel.fish6@gmail.com>
2021-03-16 07:17:24 +0800

    add more if to make the canvas more comples

--- sketch7.js
+++ sketch7.js
@@ -7,8 +7,18 @@ function setup() {
     for (var y = 0; y < height; y+=50){
       count += 1
       fill(random(200,255), random(255), random(255))
-      if (count % 2 == 0){
-        rect(x, y, 40)
+      stroke(random(200, 255), random(255), random(255))
+      if (count % 5 < 3){
+        rect(x, y, 40, 40, 5)
+        if (random() < 0.5 ){
+          fill(0)
+          noStroke()
+          ellipse(x+20, y+20, 20)
+          if (random() < 0.5){
+            fill(random(200, 255), random(255), random(255))
+            ellipse(x + 20, y + 20, 10)
+          }
+        }
       }
     }
   }
