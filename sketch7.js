2e531bc8ca38d4d52433f9a0ae9c82d381c591f1
Zhengyan Yu <noel.fish6@gmail.com>
2021-03-15 22:57:57 +0800

    draw rect when count = 2

--- sketch7.js
+++ sketch7.js
@@ -2,10 +2,14 @@ function setup() {
   createCanvas(windowWidth, windowHeight)
   background(0)
   noStroke()
-  for (var x = 0; x < width/2; x+=5){
-    for (var y = 0; y < height/2; y+=5){
+  var count = 0
+  for (var x = 0; x < width; x+=50){
+    for (var y = 0; y < height; y+=50){
+      count += 1
       fill(random(200,255), random(255), random(255))
-      rect(x, y, 40)
+      if (count % 2 == 0){
+        rect(x, y, 40)
+      }
     }
   }
 }
