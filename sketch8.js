93cfc3ef400ffbf7885c8da8a055187003e6ec82
Zhengyan Yu <noel.fish6@gmail.com>
2021-03-19 20:43:42 +0800

    use for and random functions to draw canvas

--- sketch8.js
+++ sketch8.js
@@ -8,7 +8,10 @@ function draw() {
 }
 
 function mousePressed() {
+  var count = int(random(5, 15))
+  var r = random(5, 50)
+  for(var i=0; i<count;i++){
   fill(random(255), random(200,255), random(100,200))
-  ellipse(mouseX, mouseY, 50)
-  
+  ellipse(mouseX + i*5, mouseY, r)
+  }
 }
