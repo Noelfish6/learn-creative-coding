c069634eb799337cdfd06be56d0514aa00472c31
Zhengyan Yu <noel.fish6@gmail.com>
2021-03-25 23:28:25 +0800

    add sqrt to delta

--- sketch8.js
+++ sketch8.js
@@ -10,7 +10,7 @@ function draw() {
 function mouseMoved() {
   var count = int(random(5, 15))
   var r = random(5, 50)
-  var delta = dist(pmouseX, pmouseY, mouseX, mouseY)
+  var delta = sqrt(dist(pmouseX, pmouseY, mouseX, mouseY))*5
   for(var i=0; i<count;i++){
   fill(random(255), random(200,255), random(100,200))
     ellipse(mouseX + random(-delta, delta), mouseY + random(-delta, delta), r)
