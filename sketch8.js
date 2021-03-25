5cbd165ad3ab7cabbd949d37303edfaec8484fbb
Zhengyan Yu <noel.fish6@gmail.com>
2021-03-25 23:23:35 +0800

    use another way to calculate delta

--- sketch8.js
+++ sketch8.js
@@ -10,7 +10,7 @@ function draw() {
 function mouseMoved() {
   var count = int(random(5, 15))
   var r = random(5, 50)
-  var delta = abs(pmouseX - mouseX)
+  var delta = dist(pmouseX, pmouseY, mouseX, mouseY)
   for(var i=0; i<count;i++){
   fill(random(255), random(200,255), random(100,200))
     ellipse(mouseX + random(-delta, delta), mouseY + random(-delta, delta), r)
