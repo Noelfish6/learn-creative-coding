f511fc4d60dd8c5987f58a6213298e0f665d99ef
Zhengyan Yu <noel.fish6@gmail.com>
2021-03-29 21:57:28 +0800

    generate two dimensions color circles

--- sketch9.js
+++ sketch9.js
@@ -5,7 +5,11 @@ function setup() {
 
 function draw() {
   colorMode(HSB)
-  fill(mouseX % 360, 100, 100)
-  ellipse(mouseX, mouseY, 50, 50)
-  noStroke()
+  for(var i=0; i < width; i+=50){
+    var h = map(i, 0, width, 0, 360)
+    for(var o=0; o<height; o+=50){
+      fill((h+o+frameCount)/2%360, mouseY/5, mouseX/5)
+      ellipse(i, o, 50)
+    }
+  }
 }
