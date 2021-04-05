3faecfe01c135036ae408d8a7efb6c3186b7204d
Zhengyan Yu <noel.fish6@gmail.com>
2021-04-05 15:38:31 +0800

    draw repeated tects

---
+++ sketch12.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(0)
+}
+
+function draw() {
+  translate(0, height/2)
+  for(var i=0; i<20; i++){
+    rect(0, 0, 50, 50)
+    translate(50, 0)
+  }
+}
