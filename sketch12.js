3e19d17fc41bc7c53653dda3666902b54d477108
Zhengyan Yu <noel.fish6@gmail.com>
2021-04-11 14:10:04 +0800

    add random to fill

--- sketch12.js
+++ sketch12.js
@@ -7,7 +7,7 @@ function draw() {
   translate(0, height/2)
   colorMode(HSB)
   for(var i=0; i<20; i++){
-    fill(i*10, 80, 100)
+    fill(i*10 + random(-20, 20), 80, 100)
     // rect(0, 0, 100, 100)
     rect(random(100), random(100), random(40), random(40))
     translate(120, 0)
