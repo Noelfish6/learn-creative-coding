b4984c65a908b4aaf5d31d6a7888e80fe4db0c69
Zhengyan Yu <noel.fish6@gmail.com>
2021-04-12 22:34:35 +0800

    rate the rects

--- sketch12.js
+++ sketch12.js
@@ -8,8 +8,9 @@ function draw() {
   colorMode(HSB)
   for(var i=0; i<20; i++){
     fill(i*10 + random(-20, 20), 80, 100)
-    // rect(0, 0, 100, 100)
+    rect(0, 0, 100, 100)
     rect(random(100), random(100), random(40), random(40))
     translate(120, 0)
+    rotate(0.1)
   }
 }
