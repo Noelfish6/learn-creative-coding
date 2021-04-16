a60eda2b071101d47f30ab896a478be6954701ae
Zhengyan Yu <noel.fish6@gmail.com>
2021-04-17 00:05:03 +0800

    use translate to rotate the circles

--- sketch12.js
+++ sketch12.js
@@ -4,13 +4,8 @@ function setup() {
 }
 
 function draw() {
-  translate(0, height/2)
-  colorMode(HSB)
-  for(var i=0; i<20; i++){
-    fill(i*10 + random(-20, 20), 80, 100)
-    rect(0, 0, 100, 100)
-    rect(random(100), random(100), random(40), random(40))
-    translate(120, 0)
-    rotate(0.1)
+  for(var i = 0; i<30; i++){
+    ellipse(0, 0, 50, 50)
+    translate(20, 20)
   }
 }
---
+++ sketch13.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(0)
+}
+
+function draw() {
+  translate(0, height/2)
+  colorMode(HSB)
+  for(var i=0; i<20; i++){
+    fill(i*10 + random(-20, 20), 80, 100)
+    rect(0, 0, 100, 100)
+    rect(random(100), random(100), random(40), random(40))
+    translate(120, 0)
+    rotate(0.1)
+  }
+}
