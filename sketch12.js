60eefe71485c3aad31f647dca9e317d8551c4b10
Zhengyan Yu <noel.fish6@gmail.com>
2021-04-18 22:57:29 +0800

    use lerpColor

--- sketch12.js
+++ sketch12.js
@@ -4,7 +4,12 @@ function setup() {
 }
 
 function draw() {
+  let clr1 = color("red")
+  let clr2 = color("blue")
+
   for(var i = 0; i<30; i++){
+    let midColor = lerpColor(clr1, clr2, i/30)
+    fill(midColor)
     ellipse(0, 0, 50, 50)
     translate(20, 20)
     scale(0.9)
