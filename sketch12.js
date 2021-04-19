9ae0c1bae980ce99146ebf996024ac4152f6f2f7
Zhengyan Yu <noel.fish6@gmail.com>
2021-04-19 22:49:05 +0800

    rotate the graph and save the state

--- sketch12.js
+++ sketch12.js
@@ -4,14 +4,21 @@ function setup() {
 }
 
 function draw() {
+  translate(width/2, height/2)
   let clr1 = color("red")
   let clr2 = color("blue")
-
-  for(var i = 0; i<30; i++){
-    let midColor = lerpColor(clr1, clr2, i/30)
-    fill(midColor)
-    ellipse(0, 0, 50, 50)
-    translate(20, 20)
-    scale(0.9)
+  angleMode(DEGREES)
+  
+  for(var o =0; o<8; o++){
+    rotate(o / 8 * 360)
+    push()
+    for(var i = 0; i<30; i++){
+      let midColor = lerpColor(clr1, clr2, i/30)
+      fill(midColor)
+      ellipse(0, 0, 50, 50)
+      translate(20, 20)
+      scale(0.9)
+    }
+    pop()
   }
 }
