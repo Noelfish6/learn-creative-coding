e982f7a5bfdfe109cd1ea949cd18834985f0996b
Zhengyan Yu <noel.fish6@gmail.com>
2021-04-28 21:45:10 +0800

    use shear to generate rects

---
+++ sketch14.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(0)
+}
+
+function draw() {
+  background(0, 10)
+  translate(width/2, height/2)
+  shearX(map(mouseX, 0, width, -PI/2, PI))
+  rect(0, 0, 200, 200)
+}
