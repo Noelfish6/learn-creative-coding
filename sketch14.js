f7fa62f768905ccfbe551c2fd9a8211fba227c71
Zhengyan Yu <noel.fish6@gmail.com>
2021-05-03 21:18:10 +0800

    use rotate to control the rect

--- sketch14.js
+++ sketch14.js
@@ -6,6 +6,6 @@ function setup() {
 function draw() {
   background(0, 10)
   translate(width/2, height/2)
-  shearX(map(mouseX, 0, width, -PI/2, PI))
+  rotate(map(mouseX, 0, width, -PI/2, PI))
   rect(0, 0, 200, 200)
 }
