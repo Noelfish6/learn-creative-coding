5c7759bfd277fd91ad07aa88ad78f9b1ee9d7302
Zhengyan Yu <noel.fish6@gmail.com>
2021-05-04 15:59:25 +0800

    use scale to control the rectr

--- sketch14.js
+++ sketch14.js
@@ -6,6 +6,6 @@ function setup() {
 function draw() {
   background(0, 10)
   translate(width/2, height/2)
-  rotate(map(mouseX, 0, width, -PI/2, PI))
+  scale(map(mouseX, 0, width, -PI/2, PI))
   rect(0, 0, 200, 200)
 }
